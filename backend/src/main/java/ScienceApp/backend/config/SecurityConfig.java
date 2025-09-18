4afcc4db687946352177169ed75931d0dde65267
patodemjan <patodemjan@gmail.com>
2025-09-18 23:14:31 +0200

    test - frontend and backend

---
+++ backend/src/main/java/ScienceApp/backend/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package ScienceApp.backend.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())  // vypne CSRF
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // všetko povolené
+
+        return http.build();
+    }
+}
