bf979287f7619cc0ac4e760abb651099df8d7727
patodemjan <patodemjan@gmail.com>
2025-10-07 22:24:09 +0200

    uploading routes

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './features/test/test.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // úvodná stránka s videom a kartami
-  {
-    path: 'test',
-    loadComponent: () => import('./features/test/test.component').then(m => m.TestComponent)
-  },
-  
-  
-  { path: '**', redirectTo: '' } // fallback na home
+  { path: '', component: HomeComponent }, // domovská stránka
+  { path: '**', redirectTo: '' }           // všetko iné presmeruje na home
 ];
 
 @NgModule({
