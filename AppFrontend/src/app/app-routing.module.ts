83dc7a757a5383d32f91b411c748ceb63d51a2e5
patodemjan <patodemjan@gmail.com>
2025-10-08 12:22:09 +0200

    Set HomeComponent as default route, remove TestComponent

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // hlavna stranka
+  { path: '', component: HomeComponent }, // hlavná stránka
   { path: 'visit-us', component: HomeComponent }, // placeholder
-  { path: 'create-profile', component: HomeComponent } // placeholder
+  { path: 'create-profile', component: HomeComponent }, // placeholder
+  { path: '**', redirectTo: '' } // všetky neznáme cesty presmerovať na Home
 ];
 
 @NgModule({
