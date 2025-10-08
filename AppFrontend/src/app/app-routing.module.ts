8ce5c911a6f4d964ccbb9edee12a4218ac47d262
patodemjan <patodemjan@gmail.com>
2025-10-08 15:43:57 +0200

    fix: homepage routing and redirects

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // hlavná stránka
-  { path: 'visit-us', component: HomeComponent }, // placeholder
-  { path: 'create-profile', component: HomeComponent }, // placeholder
-  { path: '**', redirectTo: '' } // všetky neznáme cesty presmerovať na Home
+  { path: '', component: HomeComponent },  // Root route
+  { path: '**', redirectTo: '' }           // Fallback route
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
