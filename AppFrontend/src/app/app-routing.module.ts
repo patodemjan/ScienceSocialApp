6f6d3252185a8b325f40fce992594d94de3ebea9
patodemjan <patodemjan@gmail.com>
2025-10-08 21:24:42 +0200

    updating angular.json

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },  // Root route
-  { path: '**', redirectTo: '' }           // Fallback route
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
