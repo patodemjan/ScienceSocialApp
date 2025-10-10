639c865c4a9db70b2e946f489147798c87607ceb
patodemjan <patodemjan@gmail.com>
2025-10-10 16:24:28 +0200

    Fix: router-outlet import and standalone setup

--- AppFrontend/src/app/app.component.ts
+++ AppFrontend/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule], // ✅ pridane RouterOutlet
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
