4ca950e6a0bde0500b3bf319f862ed3b8c4368a4
patodemjan <patodemjan@gmail.com>
2025-10-10 16:19:42 +0200

    Fix routing and build path for Render

--- AppFrontend/src/app/app.component.ts
+++ AppFrontend/src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // môže byť prázdny
-  standalone: true,
-  imports: [RouterModule, HttpClientModule]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
