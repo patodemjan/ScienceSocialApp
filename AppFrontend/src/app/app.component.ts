b57578672be792c518d582ab84cfd975f65cbe1e
patodemjan <patodemjan@gmail.com>
2025-10-12 13:23:01 +0200

    updating main.ts

--- AppFrontend/src/app/app.component.ts
+++ AppFrontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HttpClientModule], // ✅ pridane RouterOutlet
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
