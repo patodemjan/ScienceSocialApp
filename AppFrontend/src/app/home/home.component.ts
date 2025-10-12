d90b8a1529bc6bcf86831397780e0cf7859225f7
patodemjan <patodemjan@gmail.com>
2025-10-12 14:41:14 +0200

    updating changes for mobile phones

--- AppFrontend/src/app/home/home.component.ts
+++ AppFrontend/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  static apiUrl: any;
 
   constructor(private router: Router) { }
 
