5df33559e3600ffd40c14eb90e8838734f948196
patodemjan <patodemjan@gmail.com>
2025-10-13 21:44:22 +0200

    updating app-module.ts

--- AppBackend/src/main/java/com/example/AppBackend/entity/User.java
+++ AppBackend/src/main/java/com/example/AppBackend/entity/User.java
@@ -1,32 +1,39 @@
 package com.example.AppBackend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table
 public class User {
 
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	    private String username;
-	    private String password;
+    @Column(nullable = false, unique = true)
+    private String username;
 
-	    public User() {}
+    @Column(nullable = false)
+    private String password;
 
-	    public User(String username, String password) {
-	        this.username = username;
-	        this.password = password;
-	    }
+    public User() {}
 
-	    
-	    public Long getId() { return id; }
-	    public String getUsername() { return username; }
-	    public String getPassword() { return password; }
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-	    public void setId(Long id) { this.id = id; }
-	    public void setUsername(String username) { this.username = username; }
-	    public void setPassword(String password) { this.password = password; }
+    public Long getId() { return id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 	    
 }
