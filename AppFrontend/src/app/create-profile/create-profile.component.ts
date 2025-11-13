bea231a3021ede681b42f28b7d6c4e48ac12490a
patodemjan <patodemjan@gmail.com>
2025-11-13 22:44:52 +0100

    environment...

--- AppFrontend/src/app/create-profile/create-profile.component.ts
+++ AppFrontend/src/app/create-profile/create-profile.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-create-profile',
